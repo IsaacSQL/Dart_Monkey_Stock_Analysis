119a0ad95bfd3e7a37f684a6dd3df82947421f51
IsaacSQL <153321574+IsaacSQL@users.noreply.github.com>
2024-01-23 13:54:04 -0500

    Update __init__.py

--- dart_monkey/__init__.py
+++ dart_monkey/__init__.py
@@ -5,35 +5,45 @@ import time
 from .MonkeySee import MonkeySee
 from .options import options
 
-def TickerGrabber():
-    resp = requests.get('https://en.wikipedia.org/wiki/Russell_1000_Index')
+def TickerGrabber(stockindex, num):
+    resp = requests.get(stockindex)
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
 
     ticker = []
     tickers = []
-    for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[1].text
+    for row in table.findAll('tr')[num:]:
+        ticker = row.findAll('td')[num].text
         tickers.append(ticker)
     tickers = [s.replace('\n', '') for s in tickers]
     return tickers
 
 def main():
     print(""" Dart_Monkey 2024
-            _   
-        _ \'-_,#
-        _\'--','`|
-        \`---`  /
-        `----'`
+         _   
+       _ \'-_,#
+      _\'--','`|
+      \`---`  /
+       `----'`
         """)
     time.sleep(0.5)
     print("Beginning Data Scrape")
     # Create the output directory if it doesn't exist
     if not os.path.exists('output'):
         os.makedirs('output')
+    
     args = options() # Args contains command line arguments
+    
+    stockindex, num = '', 0
+    if args.index == 'sp500':
+        stockindex, num = 'https://en.wikipedia.org/wiki/S%26P_100', 0
+    elif args.index == 'russell1000':
+        stockindex, num = 'https://en.wikipedia.org/wiki/Russell_1000_Index', 1
+    else:
+        print(f'{args} is not a valid input. Try sp500, or russell1000')
+
     tickers = []    
-    tickers = TickerGrabber()
+    tickers = TickerGrabber(stockindex, num)
     for ticker in tickers:
         info = MonkeySee(ticker)
         info.toJSON()
