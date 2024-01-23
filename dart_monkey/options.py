867aeb438fde11cd9f7d96f79c443f7ab198a380
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2024-01-23 11:50:42 -0500

    Create options.py
    Create a file for passing clargs to the program

---
+++ dart_monkey/options.py
@@ -0,0 +1,17 @@
+import argparse
+
+def options():
+    # Create parser
+    parser = argparse.ArgumentParser(
+        description='Scrape data about stocks',
+        prog='dart_monkey',
+        usage='''py -m dart_monkey [INDEX]
+        Index should be sp500 for the S&P 500 or russel1000 for Russell 1000
+        '''
+    )
+    # Set options for scraper
+    parser.add_argument(
+        'index',
+        metavar='INDEX',
+        help='sp500 or russell1000'
+    )
