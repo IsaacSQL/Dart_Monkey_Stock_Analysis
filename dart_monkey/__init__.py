a4303005a5ee2f1d95950d128b1cf2b144cf1e1f
IsaacSQL <153321574+IsaacSQL@users.noreply.github.com>
2024-02-03 21:23:09 -0500

    Update __init__.py

--- dart_monkey/__init__.py
+++ dart_monkey/__init__.py
@@ -3,6 +3,7 @@ import os
 import requests
 import time
 import pandas as pd
+import numpy as np
 import json
 from .MonkeySee import MonkeySee
 from .options import options
@@ -58,16 +59,16 @@ def main():
     df = pd.DataFrame()
     # Specify the directory containing the JSON files
     directory = os.path.join(os.getcwd(), 'output')
-    # Loop over each file in the directory
+    # add the data from all the JSON files to df
     for filename in os.listdir(directory):
         if filename.endswith('.json'):  # Check if the file is a JSON file
             with open(os.path.join(directory, filename)) as f:
                 data = json.load(f)  # Load the JSON file
                 temp_df = pd.DataFrame(data, index=[0])  # Convert the dictionary to a DataFrame
                 df = df._append(temp_df, ignore_index=True)  # Append the data to the main DataFrame
-    # add the data from all the JSON files to df
-
-
+    #clean empty data
+    df.drop(df[(df.iloc[:, 1:] == 0).all(axis=1)].index, inplace=True)
+    
     ratioList = ['PriceToEarningsRatio', 'PriceToEarningsGrowth', 'ForwardPE', 'PriceToBookRatio', 'MarketCapitalization', 'ReturnOnEquity', 'ReturnOnEquity', 'DebtToEquity', 'EarningsPerShare', 'PriceToSales', 'Dividends']
     for ratio in ratioList:
         max = MonkeySee.get_max(ratio, df)
